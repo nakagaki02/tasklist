23bb145c171534f9f50713b18880d0f258df870d
ドペプシ <@DESKTOP-MHULA6N>
2021-11-27 12:03:14 +0900

    「UpdateServlet.javaを作成」

--- src/contorollers/UpdateServlet.java
+++ src/contorollers/UpdateServlet.java
@@ -1,11 +1,17 @@
 package contorollers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class UpdateServlet
@@ -19,23 +25,50 @@ public class UpdateServlet extends HttpServlet {
      */
     public UpdateServlet() {
         super();
-        // TODO Auto-generated constructor stub
-    }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
     }
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        doGet(request, response);
+
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            //セッションスコープからタスクIDを取得して
+            //該当のタスク１件のみをデータベースから取得
+
+            Task t = em.find(Task.class,(Integer)(request.getSession().getAttribute("task_id")));
+
+
+            //フォームの内容を各フィールドに上書き
+
+            String title = request.getParameter("title");
+            t.setTitle(title);
+
+            String content = request.getParameter("content");
+            t.setContent(content);
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            t.setUpdated_at(currentTime); //更新日時上書き
+
+
+            //データベースを更新
+
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            em.close();
+
+            //セッションスコープ上の不要になったデータを削除
+
+            request.getSession().removeAttribute("task_id");
+
+            //indexページへリダイレクト
+
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
     }
 
 }
